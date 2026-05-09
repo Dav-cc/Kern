fad82eabc4fb27c00f6ce6462f40a628c06f6133
Davoud Ghafouri <ghafooridavood580@gmail.com>
2026-05-10 00:45:36 +0330

    parser : now we can parse function decleration (soon can parse body . . .)

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    Lexer* lex = src_to_tkn("test.c");
+    Lexer* lex = src_to_tkn("t2.c");
+    Token* tkn = lex->head;
+    while(tkn){
+        printf("---tkn:%s---\n", tkn->value);
+        tkn = tkn->next;
+    }
+
     parse_program(lex->head);
 
 }
--- parser/parser.h
+++ parser/parser.h
@@ -5,6 +5,9 @@
 #include "../vector.h"
 #include <string.h>
 
+#define NUDE_FUNCTION 999
+
+
 typedef struct ASTNode_ {
     void* data;
     token_type type;
@@ -19,16 +22,9 @@ typedef struct func_dcl_ {
     vector* statements;
 } func_dcl;
 
-
 ASTNode* parse_program(Token* token);
 ASTNode* parse_function(Token* token);
 ASTNode* parse_var(Token* token);
 ASTNode* parse_block(Token* token);
 
-
-
-
-
-
 #endif // __KERN_PARSER_H_
-
